4e078bd3696a863d4f3f824943692619fb021232
jackdo69 <jackie.duc.do@gmail.com>
2020-03-25 18:43:36 +1100

    added header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,42 @@
 <template>
-  <h1>Header</h1>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <router-link to="/" class="navbar-brand">Stock Trader</router-link>
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          <router-link to="/portfolio" activeClass="active" tag="li"
+            ><a>Portfolio</a></router-link
+          >
+          <router-link to="/stocks" activeClass="active" tag="li"
+            ><a>Stocks</a></router-link
+          >
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">End Day</a></li>
+          <li class="dropdown">
+            <a
+              href="#"
+              class="dropdown-toggle"
+              data-toggle="dropdown"
+              role="button"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >Save  Load <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Save Data</a></li>
+                <li><a class="dropdown-item" href="#">Load Data</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
-export default {
-
-}
+  export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
