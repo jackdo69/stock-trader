dc0e80246cf8d45c3f1e6ff6e30a4cdb8c25f126
jackdo69 <jackie.duc.do@gmail.com>
2020-03-26 09:32:50 +0700

    basic set up for the stock page

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,13 +1,26 @@
 <template>
-  <h1>Stocks - Stocks component</h1>
+  <div>
+    <app-stock v-for="stock in stocks" :stock="stock"></app-stock>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+  import Stock from "./Stock.vue";
+  export default {
+    data() {
+      return {
+        stocks: [
+          { id: 1, name: "BMW", price: 110 },
+          { id: 2, name: "Google", price: 200 },
+          { id: 3, name: "Apple", price: 250 },
+          { id: 4, name: "Twitter", price: 15 }
+        ]
+      };
+    },
+    components: {
+      appStock: Stock
+    }
+  };
 </script>
 
-<style>
-
-</style>
+<style></style>
